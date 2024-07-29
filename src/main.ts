e884b0198d7ce35a56670a0fa1eaeced3d9bdb29
samir93bj <samir93bj@gmail.com>
2024-07-29 19:09:21 -0300

    chore: Update main.ts to use global validation pipe and logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './config';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('main');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      forbidNonWhitelisted: true,
+      whitelist: true,
+    }),
+  );
+
+  await app.listen(envs.port);
+
+  logger.log(`Products Microservices is running on: ${envs.port}`);
 }
 bootstrap();
