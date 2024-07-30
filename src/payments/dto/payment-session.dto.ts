bd5b4f582c5e99a5af49d81ef90fad6850dbd56f
samir93bj <samir93bj@gmail.com>
2024-07-29 22:17:06 -0300

    chore: Add PaymentSessionDto for payment session data transfer object

---
+++ src/payments/dto/payment-session.dto.ts
@@ -0,0 +1,35 @@
+import { Type } from 'class-transformer';
+import {
+  ArrayMinSize,
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+
+export class PaymentSessionDto {
+  @IsString()
+  currency: string;
+
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(() => PaymentSessionItemDto)
+  items: PaymentSessionItemDto[];
+}
+
+export class PaymentSessionItemDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsNumber()
+  @IsPositive()
+  price: string;
+
+  @IsString()
+  @IsPositive()
+  quantity: string;
+}
